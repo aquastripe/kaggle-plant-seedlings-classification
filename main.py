fee567ce9c85084a80a8a7b087066133186cce6a
Hao-Ting Li <maniac@csie.io>
2023-07-27 09:49:10 +0800

    feat: add normalization
    Took 1 minute

--- main.py
+++ main.py
@@ -186,6 +186,7 @@ def main():
     transforms = T.Compose([
         T.Resize([224, 224]),
         T.ToTensor(),
+        T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
     if not args.eval_model:
