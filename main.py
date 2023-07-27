f7436b77fdadb3f22a3240e1cbbf3b13da98a23a
Hao-Ting Li <maniac@csie.io>
2023-07-27 08:20:50 +0800

    feat: add validation and recording loss values
    Took 9 minutes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 from pathlib import Path
 
 import torch
@@ -7,28 +8,51 @@ import torch.optim as optim
 import torchvision.transforms as T
 from PIL import Image
 from torch import nn as nn
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, random_split
 from torchvision.models import resnet50, ResNet50_Weights
 from tqdm import tqdm
 
 
-def train(model, device, train_loader, optimizer, num_epochs):
-    model.train()  # for Dropout, Batch Normalization, etc.
-    for epoch in tqdm(range(num_epochs), desc='Training', ascii=True):
-        for inputs, targets in tqdm(train_loader, ascii=True):
-            inputs = inputs.to(device, non_blocking=True)
-            targets = targets.to(device, non_blocking=True)
+def train(model, train_loader, valid_loader, optimizer, num_epochs, device):
+    loss_records = {
+        'train': [],
+        'valid': [],
+    }
+    for epoch in tqdm(range(num_epochs), desc='Training Kaggle plant seedling dataset', ascii=True):
+        with torch.enable_grad():
+            model.train()  # for Dropout, Batch Normalization, etc.
+            for inputs, targets in tqdm(train_loader, ascii=True, desc='Training'):
+                inputs = inputs.to(device, non_blocking=True)
+                targets = targets.to(device, non_blocking=True)
 
-            # forward
-            outputs = model(inputs)
-            loss = F.cross_entropy(outputs, targets)
+                # forward
+                outputs = model(inputs)
+                loss = F.cross_entropy(outputs, targets)
 
-            # backward
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+                # backward
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                loss_records['train'].append(loss.item())
+
+        with torch.no_grad():
+            # validation
+            model.eval()
+            for inputs, targets in tqdm(valid_loader, ascii=True, desc='Validating'):
+                inputs = inputs.to(device, non_blocking=True)
+                targets = targets.to(device, non_blocking=True)
+
+                # forward
+                outputs = model(inputs)
+                loss = F.cross_entropy(outputs, targets)
+
+                loss_records['valid'].append(loss.item())
 
         torch.save(model.state_dict(), f'model-{epoch}.pth')
+
+    with open('loss.json', 'w', encoding='utf-8') as f:
+        json.dump(loss_records, f)
 
 
 class PlantSeedlingDataset(Dataset):
@@ -102,10 +126,12 @@ def main():
         T.Resize([224, 224]),
         T.ToTensor(),
     ])
-    train_set = PlantSeedlingDataset(args.data_root, transforms, 'train')
+    dataset = PlantSeedlingDataset(args.data_root, transforms, 'train')
+    train_set, valid_set = random_split(dataset, [0.8, 0.2])
     train_loader = DataLoader(train_set, batch_size=32, shuffle=True, num_workers=8, pin_memory=True)
+    valid_loader = DataLoader(valid_set, batch_size=32, shuffle=False, num_workers=8, pin_memory=True)
     optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
-    train(model, device, train_loader, optimizer, args.num_epochs)
+    train(model, train_loader, valid_loader, optimizer, args.num_epochs, device)
 
 
 if __name__ == '__main__':
