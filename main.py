09968edf3dc8615925ab99fde2e7bafc5c0fe2cc
Hao-Ting Li <maniac@csie.io>
2023-07-27 08:08:06 +0800

    feat: add a full training loop
    Took 8 minutes

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
+import argparse
 from pathlib import Path
 
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
+import torchvision.transforms as T
 from PIL import Image
 from torch import nn as nn
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 from torchvision.models import resnet50, ResNet50_Weights
 
 
-def train(model, device, train_loader, optimizer, epoch):
+def train(model, device, train_loader, optimizer, num_epochs):
     model.train()  # for Dropout, Batch Normalization, etc.
+    for epoch in range(num_epochs):
+        for inputs, targets in train_loader:
+            inputs = inputs.to(device, non_blocking=True)
+            target = target.to(device, non_blocking=True)
 
-    for inputs, targets in train_loader:
-        inputs = inputs.to(device, non_blocking=True)
-        target = target.to(device, non_blocking=True)
+            # forward
+            outputs = model(inputs)
+            loss = F.cross_entropy(outputs, targets)
 
-        # forward
-        outputs = model(inputs)
-        loss = F.cross_entropy(outputs, targets)
+            # backward
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-        # backward
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        torch.save(model.state_dict(), f'model-{epoch}.pth')
 
 
 class PlantSeedlingDataset(Dataset):
@@ -69,18 +73,6 @@ class PlantSeedlingDataset(Dataset):
         return len(self.image_paths)
 
 
-def main():
-    model = Model()
-    device = torch.device('cuda')
-    train_set = ...
-    train_loader = ...
-    train(model, device, train_loader, optimizer, epoch)
-
-
-if __name__ == '__main__':
-    main()
-
-
 class Model(nn.Module):
 
     def __init__(self):
@@ -91,3 +83,29 @@ class Model(nn.Module):
     def forward(self, inputs):
         outputs = self.model(inputs)
         return outputs
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_root', type=str)
+    parser.add_argument('--num_epochs', type=int, default=10)
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    device = torch.device('cuda')
+    model = Model()
+    model.to(device)
+    transforms = T.Compose([
+        T.Resize([224, 224]),
+        T.ToTensor(),
+    ])
+    train_set = PlantSeedlingDataset(args.data_root, transforms, 'train')
+    train_loader = DataLoader(train_set, batch_size=32, shuffle=True, num_workers=8, pin_memory=True)
+    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+    train(model, device, train_loader, optimizer, args.num_epochs)
+
+
+if __name__ == '__main__':
+    main()
